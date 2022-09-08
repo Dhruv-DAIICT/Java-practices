ff356764ac3993a565968cbf23675526545eef12
Dhruv-DAIICT <112800862+Dhruv-DAIICT@users.noreply.github.com>
2022-09-08 13:51:19 +0530

    initial commit

---
+++ video1.java
@@ -0,0 +1,32 @@
+class FirstClass{
+    public static void main(String args[]){
+        System.out.println("Hello world");
+    }
+}
+
+
+//why i have started with java
+// so first reasone is that i am intresterd into web development and cp both
+// second is i  m using diffrent platform and java is plateform independent
+
+//jdk(development tools + (JRE (libreries + jvm) ))
+// compilation
+// source code (.java) -> compiler (jdk) -> Byte code (.class)  (and this system independent/ portable language)
+// Execution -> conversion in native code (type of binary code)
+// byte code -> JVM -> native code
+
+//function
+/*
+ * void main(){ //void is return type return nothing 
+ * }
+ */
+
+
+ //class  // can contain more than one function for ex. a kitchen contains many boxes
+ /*
+  * class Main(){
+    void main(){
+
+    }
+  }
+  */
